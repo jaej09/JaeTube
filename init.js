3408026b300d9cac3334b72130a2c272227658db
Jae Jin <jaej09@outlook.com>
2020-09-03 03:35:26 +0900

    #2.8 Express Core: Routing

---
+++ init.js
@@ -0,0 +1,6 @@
+import app from './app'; // app.js에서 선언했던 app object 사용
+
+const PORT = 4000;
+const handleListening = () => console.log(`✅ Listening on: http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
