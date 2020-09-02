f1e4f2b0e74f4de9341b6ebacc3fdcc84c4a3a68
Jae Jin <jaej09@outlook.com>
2020-09-03 03:19:33 +0900

    #2.7 Express Core: Middlewares

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-const express = require('express');
+import express from 'express'; // var express = require('express');
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import helmet from 'helmet';
+
 const app = express();
 
-function handleListening() {
-	console.log('listening on: http://localhost:4000');
-}
+// declare variables
+const PORT = 4000;
 
-app.listen(4000, handleListening);
+const handleListening = () => console.log(`✅ Listening on: http://localhost:${PORT}`);
+
+const handleHome = (req, res) => res.send('Hello from Home');
+const handleProfile = (req, res) => res.send('Hello from Profile');
+
+// Middleware - Order matters
+app.use(helmet()); // Help secure Express/Connect apps with various HTTP headers
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', handleHome);
+app.get('/profile', handleProfile);
+
+app.listen(PORT, handleListening);
