241ffc569dfaa665aaa0d934edd5d31520b60446
Jae Jin <jaej09@outlook.com>
2020-09-14 00:47:38 +0900

    #3.1 Connecting to MongoDB

--- db.js
+++ db.js
@@ -1,58 +1,10 @@
-export const videos = [
-	{
-		id          : 96337,
-		title       : 'Lorem Ipsum',
-		description :
-			'Boy favourable day can introduced sentiments entreaties. Noisier carried of in warrant because. So mr plate seems cause chief widen first. Two differed husbands met screened his. Bed was form wife out ask draw. Wholly coming at we no enable. Offending sir delivered questions now new met. Acceptance she interested new boisterous day discretion celebrated. ',
-		views       : 24,
-		poster      : 'https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217',
-		videoFile   : 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-		creator     : {
-			id    : 121212,
-			name  : 'Jae',
-			email : 'jae@eoiw.com'
-		}
-	},
-	{
-		id          : 33219,
-		title       : 'Lorem Ipsum',
-		description :
-			'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempus metus finibus ex cursus posuere. ',
-		views       : 24,
-		poster      : 'https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217',
-		videoFile   : 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-		creator     : {
-			id    : 121212,
-			name  : 'Jae',
-			email : 'jae@eoiw.com'
-		}
-	},
-	{
-		id          : 70536,
-		title       : 'Lorem Ipsum',
-		description :
-			'As collected deficient objection by it discovery sincerity curiosity. Quiet decay who round three world whole has mrs man. Built the china there tried jokes which gay why. Assure in adieus wicket it is. But spoke round point and one joy. Offending her moonlight men sweetness see unwilling. Often of it tears whole oh balls share an. ',
-		views       : 24,
-		poster      : 'https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217',
-		videoFile   : 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-		creator     : {
-			id    : 121212,
-			name  : 'Jae',
-			email : 'jae@eoiw.com'
-		}
-	},
-	{
-		id          : 49638,
-		title       : 'Lorem Ipsum',
-		description :
-			'Parish so enable innate in formed missed. Hand two was eat busy fail. Stand smart grave would in so. Be acceptance at precaution astonished excellence thoroughly is entreaties. Who decisively attachment has dispatched. Fruit defer in party me built under first. Forbade him but savings sending ham general. So play do in near park that pain. ',
-		views       : 24,
-		poster      : 'https://peach.blender.org/wp-content/uploads/title_anouncement.jpg?x11217',
-		videoFile   : 'https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4',
-		creator     : {
-			id    : 121212,
-			name  : 'Jae',
-			email : 'jae@eoiw.com'
-		}
-	}
-];
+import mongoose from 'mongoose';
+
+mongoose.connect('mongodb://localhost:27017/youtube-clone', { useNewUrlParser: true, useUnifiedTopology: false });
+
+const db = mongoose.connection;
+const handleOpen = () => console.log('✅ Connected to DB');
+const handleError = (err) => console.log(`❌ Error on DB connection: ${err}`);
+
+db.once('open', handleOpen);
+db.on('error', handleError);
--- init.js
+++ init.js
@@ -1,3 +1,4 @@
+import './db';
 import app from './app'; // app.js에서 선언했던 app object 사용
 
 const PORT = 4000;
