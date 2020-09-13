74845879b04c0b25c7f1df08f63f6f9415f1be08
Jae Jin <jaej09@outlook.com>
2020-09-14 00:05:38 +0900

    #2.24 Log In and User Profile Controller

--- middlewares.js
+++ middlewares.js
@@ -3,5 +3,6 @@ import routes from './routes';
 export const localsMiddleware = (req, res, next) => {
 	res.locals.siteName = 'WeTube';
 	res.locals.routes = routes;
+	res.locals.user = { isAuthenticated: true, id: 1 };
 	next(); // 미들웨어기 때문에, next()를 실행하지 않으면 다음 순서로 넘어가지 못한다.
 };
