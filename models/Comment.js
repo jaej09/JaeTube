7b85607fb225be1cd28d97457398449ac6f27918
Jae Jin <jaej09@outlook.com>
2020-09-18 00:14:35 +0900

    #3.4 Comment Model
    Sava Video Object ID in Comment Schema When a Comment Is Created.

---
+++ models/Comment.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongooses';
+
+const CommentSchema = new mongoose.Schema({
+	text     : {
+		type     : String,
+		required : 'Text is required'
+	},
+	createAt : {
+		type    : Date,
+		default : Date.now
+	},
+	video    : {
+		type : mongoose.Schema.Types.ObjectId,
+		ref  : Video
+	}
+});
