abdcd651b242672e9d0e83fbdb195d9bb49d2a98
Jae Jin <jaej09@outlook.com>
2020-10-01 21:18:06 +0900

    #6.2 Local Authentication with Passport

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,18 +1,23 @@
 import routes from '../routes';
+import User from '../models/User';
 
 export const getJoin = (req, res) => {
   res.render('join', { pageTitle: 'Join' });
 };
 
-export const postJoin = (req, res) => {
+export const postJoin = async (req, res) => {
   const { body: { name, email, password, verifyPassword } } = req;
   if (password !== verifyPassword) {
     res.status(400); // Bad Request -> 브라우저에서 Bad Request를 인지하면 아이디, 비번을 저장할 것인지 묻지 않는다.
     res.render('join', { pageTitle: 'Join' });
   }
   else {
-    // TODO: Register User
-    // TODO: Log user in
+    try {
+      const user = await User({ name, email }); // User.create를 사용하면 {}안에 있는 내용을 생성시킨 다음 데이터베이스에 저장하기 때문에, 그냥 User 사용했다.
+      await User.register(user, password);
+    } catch (err) {
+      console.log(err);
+    }
     res.redirect(routes.home);
   }
 };
