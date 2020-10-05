64562ba3feb4b29760f69634bb24ebdd9d2600a4
Jae Jin <jaej09@outlook.com>
2020-10-05 23:34:36 +0900

    Add styles with bootstrap

--- middlewares.js
+++ middlewares.js
@@ -9,7 +9,7 @@ const multerVideo = multer({ dest: 'uploads/videos/' });
 export const uploadVideo = multerVideo.single('videoFile');
 
 export const localsMiddleware = (req, res, next) => {
-  res.locals.siteName = 'WeTube';
+  res.locals.siteName = 'Youtube Clone';
   res.locals.routes = routes;
   res.locals.loggedUser = req.user || null; // passport로 부터 생성된 user가 존재하지 않으면 빈 object 생성
   return next(); // 미들웨어기 때문에, next()를 실행하지 않으면 다음 순서로 넘어가지 못함
