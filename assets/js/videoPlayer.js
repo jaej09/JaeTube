1bb0a784c1319982493236bd7b6113828b0c7a51
Jae Jin <jaej09@outlook.com>
2020-10-16 01:57:02 +0900

    #8.2 Mute Unmute

--- assets/js/videoPlayer.js
+++ assets/js/videoPlayer.js
@@ -1,14 +1,36 @@
 var VideoPlayer = (function() {
   let $videoContainer, $video, $volumeBtn, $playBtn, $expandBtn;
 
+  /**
+   * https://developer.mozilla.org/en-US/docs/Web/API/HTMLMediaElement
+   * 여기서 자세한 필요한 기능을 찾으면 됨
+   * 
+   */
+
   function handlePlayBtnClick() {
-    if ($video.paused) $video.play();
-    else $video.pause();
+    if ($video.paused) {
+      $video.play();
+      $playBtn.innerHTML = '<i class="fas fa-pause"></i>';
+    }
+    else {
+      $video.pause();
+      $playBtn.innerHTML = '<i class="fas fa-play"></i>';
+    }
+  }
+
+  function handleVolumeBtnClick() {
+    if ($video.muted) {
+      $video.muted = false;
+      $volumeBtn.innerHTML = '<i class="fas fa-volume-up"></i>';
+    }
+    else {
+      $video.muted = true;
+      $volumeBtn.innerHTML = '<i class="fas fa-volume-mute"></i>';
+    }
   }
 
   function init() {
     $videoContainer = document.querySelector('.video-player');
-    console.log('ok');
 
     if ($videoContainer) {
       $video = $videoContainer.querySelector('video');
@@ -17,6 +39,7 @@ var VideoPlayer = (function() {
       $expandBtn = $videoContainer.querySelector('.button-expand');
 
       $playBtn.addEventListener('click', handlePlayBtnClick);
+      $volumeBtn.addEventListener('click', handleVolumeBtnClick);
     }
   }
 
