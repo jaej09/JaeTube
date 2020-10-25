c7e19572bc35120b9e9c3af676ba02570f09964b
Jae Jin <jaej09@outlook.com>
2020-10-26 00:10:38 +0900

    #10.4 API Adding a Comment

--- assets/js/addComment.js
+++ assets/js/addComment.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import passport from 'passport';
 
 var AddComment = (function() {
-  let $form, $input;
+  let $form, $input, $commentContainer, $commentCount;
 
   async function sendComment(comment) {
     const videoId = window.location.href.split('/videos/')[1];
@@ -13,7 +14,30 @@ var AddComment = (function() {
       }
     });
 
-    console.log(response);
+    if (response.status === 200) fakeComment(comment);
+  }
+  function increaseNumber() {
+    $commentCount.innerHTML = parseInt($commentCount.innerHTML, 10) + 1;
+  }
+
+  function fakeComment(comment) {
+    const $media = document.createElement('div');
+    const $img = document.createElement('img');
+    const $mediaBody = document.createElement('div');
+    const $p = document.createElement('p');
+    $p.innerHTML = comment;
+
+    $media.classList.add('media');
+    $img.classList.add('align-self-start');
+    $img.classList.add('mr-3');
+    $mediaBody.classList.add('media-body');
+
+    $mediaBody.appendChild($p);
+    $media.appendChild($img);
+    $media.appendChild($mediaBody);
+    $commentContainer.prepend($media); // 제일 앞에 오기를 원하면, prepend VS 제일 밑에 나오길 원하는 .append
+
+    increaseNumber();
   }
 
   function handleSubmit(evt) {
@@ -31,6 +55,8 @@ var AddComment = (function() {
 
     if ($form) {
       $input = $form.querySelector('input');
+      $commentContainer = document.querySelector('.video-detail__comments-list');
+      $commentCount = document.querySelector('.video__comment-number');
       $form.addEventListener('submit', handleSubmit);
     }
   }
